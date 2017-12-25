cfe66abe235c32d631b97bebb74ba08729ac35a1
Andrew Wittrock <awittrock@gmail.com>
2017-12-25 00:08:52 -0800

    props.children lesson.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// class App extends React.Component {
-//   constructor(){
-//     super();
-//     this.state = {
-//       txt: 'this is the state txt',
-//       cat: 0
-//     }
-//   }
-//   update(e){
-//     this.setState({txt: e.target.value})
-//   }
-//   render(){
-//     return (
-//       <div>
-//         <input type="text" onChange={this.update.bind(this)}/>
-//         <h1>{this.state.txt}</h1>
-//         <Widget update={this.update.bind(this)} />
-//       </div>
-//     )
-//   }
-// }
-
-// const Widget = (props) =>
-//   <input type="text" onChange={props.update}/>
-
 class App extends React.Component {
   render(){
-    return <button>I <Heart /> React</button>
+    return <Button>I <Heart /> React</Button>
   }
 }
 
