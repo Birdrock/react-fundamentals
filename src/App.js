07c8fcd44df5997990138c6b8bbf20288402fac3
Andrew Wittrock <awittrock@gmail.com>
2017-12-24 23:35:35 -0800

    Accessing nested data in react.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// class App extends React.Component {
+//   constructor(){
+//     super();
+//     this.state = {
+//       txt: 'this is the state txt',
+//       cat: 0
+//     }
+//   }
+//   update(e){
+//     this.setState({txt: e.target.value})
+//   }
+//   render(){
+//     return (
+//       <div>
+//         <input type="text" onChange={this.update.bind(this)}/>
+//         <h1>{this.state.txt}</h1>
+//         <Widget update={this.update.bind(this)} />
+//       </div>
+//     )
+//   }
+// }
+
+// const Widget = (props) =>
+//   <input type="text" onChange={props.update}/>
+
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      txt: 'this is the state txt',
-      cat: 0
-    }
-  }
-  update(e){
-    this.setState({txt: e.target.value})
-  }
   render(){
-    return (
-      <div>
-        <input type="text" onChange={this.update.bind(this)}/>
-        <h1>{this.state.txt}</h1>
-        <Widget update={this.update.bind(this)} />
-      </div>
-    )
+    return <button>I <Heart /> React</button>
   }
 }
 
-const Widget = (props) =>
-  <input type="text" onChange={props.update}/>
+const Button = (props) => <button>{props.children}</button>
+
+class Heart extends React.Component {
+  render(){
+    return <span>&hearts;</span>
+  }
+}
 
 export default App
